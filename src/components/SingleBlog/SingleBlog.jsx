9fce48888e9df32f8e63452615baed94d70d33d7
istiaq-tanim <istiaq.tanim17@gmail.com>
2023-03-31 11:36:33 +0600

    author image added

--- src/components/SingleBlog/SingleBlog.jsx
+++ src/components/SingleBlog/SingleBlog.jsx
@@ -10,15 +10,15 @@ const SingleBlog = (props) => {
         <div className='my-3'>
             <div className="card w-full bg-base-100 shadow-xl">
                 <figure><img className='h-[400px]' src={picture} alt="Shoes"/></figure>
-                <div className='flex justify-between mt-5'>
-                    <div className='flex md:gap-4'>
-                        <img className="h-16 w-16 md:mx-auto rounded-full" src={authorImage} />
+                <div className='md:flex md:justify-between mt-5'>
+                    <div className='md:flex md:gap-4'>
+                        <img className="h-16 w-16 md:mx-auto rounded-full inline-block" src={authorImage} />
                         <div>
-                            <p className="text-xl text-[#111111] font-semibold">{name}</p>
-                            <p className='text-[rgba(17,17,17,0.6)]'>{publish}</p>
+                            <p className="text-xl pt-1 text-[#111111] font-semibold">{name}</p>
+                            <p className='text-[rgba(17,17,17,0.6)] pt-1'>{publish}</p>
                         </div>
                     </div>
-                    <p className='text-lg text-[rgba(17,17,17,0.6)]'>{time} min read <span onClick={()=>{handleBookMark (props.blog) }} className='px-2'><FontAwesomeIcon icon={faBookmark} /></span></p>
+                    <p className='text-lg text-[rgba(17,17,17,0.6)]'>{time} min read <span onClick={()=>{handleBookMark (props.blog) }} className='px-2 cursor-pointer'><FontAwesomeIcon icon={faBookmark} /></span></p>
 
                 </div>
                 <h3 className="text-left my-5  text-[#111111] font-bold text-2xl">{title}</h3>
