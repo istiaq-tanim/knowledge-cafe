1965929220c6bfdaff61d32ee7dd1f036781301a
istiaq-tanim <istiaq.tanim17@gmail.com>
2023-04-01 11:08:34 +0600

    check again

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
                 <div className="dropdown dropdown-end">
                     <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full">
-                            <img src="../../../public/images/Ellipse 1.png"/>
+                            <img src="https://img.freepik.com/free-photo/business-concept-portrait-confident-young-businesswoman-keeping-arms-crossed-looking-camera-w_1258-104422.jpg?w=996&t=st=1680325668~exp=1680326268~hmac=4de26b680d6843a8b205f64d72f19e969f3fc44931e0f6c8567e9eac5dc2c6f5"/>
                         </div>
                     </label>
                 </div>
